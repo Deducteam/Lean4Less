4df97e418d2161f5a198bb7a6a62f61510ee3886
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2026-05-27 20:55:03 -0700

    stable build

--- lakefile.lean
+++ lakefile.lean
@@ -5,12 +5,12 @@ package lean4less
 
 require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.18.0-rc1"
 
--- require lean4lean from "/home/rvaishna/projects/lean4lean/"
+-- require lean4lean from "/home/rish/lean4lean/"
 
-require lean4lean from git "https://github.com/rish987/lean4lean" @ "lean4less"
+require lean4lean from git "https://github.com/rish987/lean4lean" @ "stable"
 
 require Cli from git
-  "https://github.com/leanprover/lean4-cli" @ "main"
+  "https://github.com/leanprover/lean4-cli" @ "v4.18.0-rc1"
 
 @[default_target]
 lean_lib patch { globs := #[Glob.submodules `patch] }
